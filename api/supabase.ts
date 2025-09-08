08d2971d6530d16d277a8ff69240a6ea6916f2e1
AxelLarrea <axel.larrea.9@gmail.com>
2025-09-08 19:36:17 -0300

    Add serveless function to delete users

---
+++ api/supabase.ts
@@ -0,0 +1,15 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.SUPABASE_URL!
+const supabaseAdminKey = process.env.SUPABASE_ADMIN_KEY!
+
+export const supabaseAdmin = createClient(
+  supabaseUrl, 
+  supabaseAdminKey,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false
+    }
+  }
+)
